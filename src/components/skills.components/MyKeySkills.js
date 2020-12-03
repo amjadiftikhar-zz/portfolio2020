199d43f1e5a4c6682b2f6cba9e007d81786e1adc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-12-03 22:21:09 +0100

    MyKeySkills component added

---
+++ src/components/skills.components/MyKeySkills.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Card } from 'reactstrap';
+import Style from "./skills.style";
+
+const skillsArr = ["Javascript", "Typescript", "React","Storybook", "HTML5", 
+    "CSS3", "Node JS", "Bootstrap","Material UI", "Express", "NPM", "Webpack",
+    "MySql", "Github", "Postman", "Heroku"]
+
+function MyKeySkills() {
+    return (
+        <Style>
+            <div className="skillsList">
+                {skillsArr.map(skill => 
+                <Card border="info" style={{margin:"1.5rem"}} 
+                    className="col-md- col-sm-2 offset-md-1"> 
+                    <span key={skill} className="sectionElement">
+                        {skill}
+                    </span>
+                </Card>  
+                )}
+            </div> 
+        </Style>
+    )
+}
+
+export default MyKeySkills;
