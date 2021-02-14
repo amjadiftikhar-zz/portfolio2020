8289415b2ef7a36cc1669a29500c8ca4336a1f5c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-02-14 22:52:28 +0100

    animation implemented

--- src/components/chart.components/PieChart.js
+++ src/components/chart.components/PieChart.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import * as d3 from "d3";
 import {Slice} from "./Slice";
 
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
 export const PieChart = (props) => {
+    useEffect(() => {
+       Aos.init({duration: 3000})
+    }, [])
+
     // initializing pie
     let pie = d3
         .pie()
@@ -18,7 +25,7 @@ export const PieChart = (props) => {
     const colors = d3.scaleOrdinal(d3.schemeCategory10)
 
     return (        
-        <svg height={props.height } width={props.width}>
+        <svg height={props.height } width={props.width} data-aos='fade-up'>
             {/* using grouping element tag */}
             <g transform={`translate(${props.outerRadius},
                 ${props.outerRadius})`}
