1fa29321e3e8ac4bef2ffd28feba498d0be6445e
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-13 00:47:31 +0100

    json data imported

--- src/components/skills.components/MyKeySkills.js
+++ src/components/skills.components/MyKeySkills.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Card } from 'reactstrap';
+import skillData from "../../assets/data.json";
 import Style from "./skills.style";
-
-const skillsArr = ["Javascript", "Typescript", "React","Storybook", "HTML5", 
-    "CSS3", "Node JS", "Bootstrap","Material UI","D3.js", "Unit Testing", "Express", "NPM", "Webpack",
-    "MySql", "Github", "Postman", "Heroku"]
 
 function MyKeySkills() {
     return (
         <Style>
             <div className="skillsList">
-                {skillsArr.map((skill, index) => 
+                {skillData.skillsArr.map((skill, index) => 
                 <Card key={index} border="info" style={{margin:"1.5rem"}} 
                     className="col-md- col-sm-3 offset-md-1"> 
                     <span  className="sectionElement">
