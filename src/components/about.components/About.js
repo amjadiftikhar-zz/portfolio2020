5d92e9c5ff51defaba9d8616c64d62bf006be599
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-19 23:52:34 +0100

    id added for test purpose

--- src/components/about.components/About.js
+++ src/components/about.components/About.js
@@ -7,7 +7,7 @@ import "./about.style.css";
 
 export default function About() {
   return (
-    <Container style={{marginTop:"3rem"}}>
+    <Container style={{marginTop:"3rem"}} data-testid='about'>
       <h3><BsFillInfoSquareFill className="aboutMeIcon"/>About me</h3>
       <div className="aboutMeSection">
         <div className="imageSection">
