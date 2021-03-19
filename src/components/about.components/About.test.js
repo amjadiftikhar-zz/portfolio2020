f9746834cbda4706b32faf51e0e1156f699f8c00
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-19 23:53:08 +0100

    About test added

---
+++ src/components/about.components/About.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from './About';
+
+test('About renders', () => {
+  const {getByTestId} = render(<About/>);
+  const linkElement = getByTestId('about');
+  expect(linkElement).toBeTruthy()
+})
