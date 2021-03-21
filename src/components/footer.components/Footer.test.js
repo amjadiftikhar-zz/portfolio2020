ef3aab9d88a8431ac68420e0adc1825ce889a2ee
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-21 22:30:16 +0100

    Footer test added

---
+++ src/components/footer.components/Footer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+
+test('Footer renders', () => {
+  const {getByTestId} = render(<Footer/>);
+  const linkElement = getByTestId('footer');
+  expect(linkElement).toBeTruthy()
+})
