d3dff195c4048c5f378d1423f08964575a9921cd
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-21 22:58:09 +0100

    id added for test purpose

--- src/components/skills.components/SkillsCard.js
+++ src/components/skills.components/SkillsCard.js
@@ -3,7 +3,8 @@ import {Card} from "react-bootstrap";
 
 function SkillsCard(props) {
     return (        
-        <Card className="skillCard" style={{ width: '18rem', marginBottom:"5%"}}>
+        <Card className="skillCard" style={{ width: '18rem', marginBottom:"5%"}} 
+          data-testid='skillsCard'>
           <Card.Img variant="top" style={{width:"8rem", height:"8rem", 
             margin:"auto"}} src={props.imageSrc} />
           <Card.Body>
