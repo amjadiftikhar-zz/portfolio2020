85474e7b64c60c59f4b43cd512552ef8457546ef
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-26 22:59:00 +0100

    testid added for test purpose

--- src/components/contactUs.components/ContactForm.js
+++ src/components/contactUs.components/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ContactForm(props) {
   return (
-    <form className="form-control-sm" onSubmit={props.sendEmail}>
+    <form className="form-control-sm" onSubmit={props.sendEmail} data-testid='contactFormId'>
       <div className="form-group row">
         <label className="col-sm-2 col-form-label col-form-label-sm">
           Phone</label>
