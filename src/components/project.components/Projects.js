1e78f02aeca0e8aa5cd37f4d8246688b1c817b9f
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-27 23:59:38 +0100

    changes made

--- src/components/project.components/Projects.js
+++ src/components/project.components/Projects.js
@@ -10,20 +10,20 @@ const Projects = () => {
       <Container>
         <h3><GrProjects className="projectIcon"/>Projects</h3>
         <div className="projectSection">      
-          {projectsData.projects.map((proj, index) => <Project 
-            key={index}
-            projectTitle={proj.projectTitle}
-            stackList={proj.stackList.map(techStack => 
-              <div key={techStack} className="techElements">
-                {techStack}
-              </div>
-            )} 
-            projectImage={proj.projectImage}
-            githubLink={proj.githubLink}
-            herokuLink={proj.herokuLink}
-            description={proj.description}
-          />
-          )}
+          { projectsData.projects.map((proj, index) => <Project 
+              key={index}
+              projectTitle={proj.projectTitle}
+              stackList={proj.stackList.map(techStack => 
+                <div key={techStack} className="techElements">
+                  {techStack}
+                </div>
+              )} 
+              projectImage={proj.projectImage}
+              githubLink={proj.githubLink}
+              herokuLink={proj.herokuLink}
+              description={proj.description}
+            />)
+          }
         </div>
       </Container> 
     )
