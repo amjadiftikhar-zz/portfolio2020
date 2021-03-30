aad22d12f8fde9f49b20c0409d4c03b3fe005556
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-03-30 23:10:56 +0200

    renamed

--- src/components/accordion.components/Accordion-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Accordion from './Accordion';
-
-test('Accordion renders', () => {
-  const {getByTestId} = render(<Accordion/>);
-  const linkElement = getByTestId('accordion');
-  expect(linkElement).toBeTruthy()
-})
