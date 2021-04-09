e1f580f4b8b148f731c353c08dd069c4e58a313c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-04-09 23:16:25 +0200

    class added

--- src/components/education.components/Education.js
+++ src/components/education.components/Education.js
@@ -15,7 +15,7 @@ function Education() {
             <div className="educationSection">
                 <div className="educationContents">
                     <p className="pItalic">Institute</p>
-                    <h5>Hack Your Future Copenhagen</h5>
+                    <h5 className="placeName">Hack Your Future Copenhagen</h5>
                     <p className="pItalic">Acquired skills are, HTML/CSS, 
                         JavaScript, <br/>Node JS, MySql og React</p>
                     <p className="pItalic">08/2019 – 04/2020</p>                    
