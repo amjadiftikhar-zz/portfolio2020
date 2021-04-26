4117750f7608c8e6a588292b76ce104fb98a227d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-04-26 16:32:52 +0200

    PrintEducation component added

---
+++ src/components/print.components/PrintEducation.js
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react'
+import ReactToPrint from "react-to-print";
+import Education from '../education.components/Education';
+
+function PrintEducation(props) {
+    const componentRef = useRef();
+    return (
+        <div>
+            <ReactToPrint 
+                trigger={() => <a className="downloadClass"> Download resume! </a>}
+                content={() => componentRef.current}
+            />
+            <div className="printSource" ref={componentRef}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default PrintEducation
+
+
+
+
+
+
+
+// import React, {useRef} from 'react'
+// import ReactToPrint from "react-to-print";
+// import Education from '../education.components/Education';
+
+// function PrintEducation() {
+//     const componentRef = useRef();
+//     return (
+//         <div>
+//             <ReactToPrint 
+//                 trigger={() => <a className="downloadClass"> Download! </a>}
+//                 content={() => componentRef.current}
+//             />
+//             <div className="printSource" ref={componentRef}>
+//                 <Education  />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default PrintEducation
