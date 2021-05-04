900e4c35030c32707eaa8ae22b69a20931306b4c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-05-04 23:05:29 +0200

    code cleaned

--- src/components/layout.components/Layout.js
+++ src/components/layout.components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
 
-export default function Layout(props) {
+function Layout(props) {
     return (
        <Container fluid={true} style={{marginTop:"10rem"}} data-testid='layout'>
            {props.children}
        </Container>
     )
 }
+
+export default Layout;
