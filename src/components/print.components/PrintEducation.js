894c79309f9310049e9cb52423330fd809a6fd39
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-05-06 12:32:13 +0200

    code cleaned

--- src/components/print.components/PrintEducation.js
+++ src/components/print.components/PrintEducation.js
@@ -26,23 +26,3 @@ export default PrintEducation;
 
 
 
-// import React, {useRef} from 'react'
-// import ReactToPrint from "react-to-print";
-// import Education from '../education.components/Education';
-
-// function PrintEducation() {
-//     const componentRef = useRef();
-//     return (
-//         <div>
-//             <ReactToPrint 
-//                 trigger={() => <a className="downloadClass"> Download! </a>}
-//                 content={() => componentRef.current}
-//             />
-//             <div className="printSource" ref={componentRef}>
-//                 <Education  />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default PrintEducation
