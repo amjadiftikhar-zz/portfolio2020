093623fc05261c788b218b8806a1ece0a403662d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-05-26 22:35:19 +0200

    changes made

--- src/components/about.components/About.test.js
+++ src/components/about.components/About.test.js
@@ -5,5 +5,5 @@ import About from './About';
 test('About renders', () => {
   const {getByTestId} = render(<About/>);
   const linkElement = getByTestId('about');
-  expect(linkElement).toBeTruthy()
-})
+  expect(linkElement).toBeTruthy();
+});
