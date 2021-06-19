218e574f59fbad3223f1fd4f6ade12ca41efcd25
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-06-20 00:02:19 +0200

    changes made

--- src/components/education.components/education.style.js
+++ src/components/education.components/education.style.js
@@ -11,17 +11,20 @@ const Style = styled.div`
     .eduTitle {
         margin-bottom: 1.5rem;
     }
-    
+
     .educationIcon {
         margin-right: 0.6vw;
         font-size: 1.6vw;
     }
+
     .subTitle {
         font-size: 1.2rem;
     }
+
     .placeName {
         font-size: 1.1rem;
     }
+    
     .educationSection {
         display: flex;
         justify-content: space-around;
