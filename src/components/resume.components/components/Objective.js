2c82687be5f1caa014bad1371f5db9c101f261cd
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-13 00:21:03 +0200

    changes made

--- src/components/resume.components/components/Objective.js
+++ src/components/resume.components/components/Objective.js
@@ -6,7 +6,12 @@ const Objective = () => {
         <div className="objectiveSection">
             <h1 className="myName">Amjad Iftikhar</h1>
             <h2 className="jobTitle">Frontend Developer</h2>
-            <p>Curious and talented developer with a flair for creating elegant solutions in the least amount of time and have passion for responsive website design as well as a firm believer in the mobile-first approach. Have contributed in different projects to learn new development techniques and very passionate about new fields in the domain of tech and development.
+            <p>
+                Curious and talented developer with a flair for creating elegant 
+                solutions in the least amount of time and have passion for responsive 
+                website design as well as a firm believer in the mobile-first approach. 
+                Have contributed in different projects to learn new development techniques 
+                and very passionate about new fields in the domain of tech and development.
             </p>
         </div>
     )
