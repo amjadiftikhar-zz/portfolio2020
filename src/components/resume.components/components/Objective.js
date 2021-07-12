e174927eac9902fb2e439fe5cec61479235c40c8
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-13 00:07:54 +0200

    components folder added under resume.components

---
+++ src/components/resume.components/components/Objective.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './resume.styles.css';
+
+const Objective = () => {
+    return (
+        <div className="objectiveSection">
+            <h1 className="myName">Amjad Iftikhar</h1>
+            <h2 className="jobTitle">Frontend Developer</h2>
+            <p>Curious and talented developer with a flair for creating elegant solutions in the least amount of time and have passion for responsive website design as well as a firm believer in the mobile-first approach. Have contributed in different projects to learn new development techniques and very passionate about new fields in the domain of tech and development.
+            </p>
+        </div>
+    )
+}
+
+export default Objective;
