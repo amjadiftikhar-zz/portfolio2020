e0600baf3144123a3781c805da93cac45f8290c1
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-14 23:25:30 +0200

    Profile components added

---
+++ src/components/resume.components/components/profile.components/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import profilePic from "../../../../images/profilePic.png";
+import {IoIosMail} from "react-icons/io";
+import {GoDeviceMobile} from "react-icons/go";
+import {DiWebplatform} from "react-icons/di";
+
+import './profile.styles.css';
+
+
+const Profile = () => {
+
+    return (
+        <div className='profileImageSection'>
+            <img className='profilePicture' src={profilePic} alt="" />
+            <div className='contactDetails'> 
+                <span><IoIosMail className="contactIcons" />amjadiftikhar_99@hotmail.com</span>
+                <span><GoDeviceMobile className="contactIcons"/>+4552920945</span>
+                <span><DiWebplatform className="contactIcons"/>https://amjadiftikhar.netlify.app/</span> 
+            </div>          
+        </div>
+    )
+}
+
+export default Profile;
