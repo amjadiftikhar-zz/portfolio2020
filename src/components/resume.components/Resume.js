92cc3c18cb10587c7ad1a8690828304aa7478244
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-17 01:22:48 +0200

    changes made

--- src/components/resume.components/Resume.js
+++ src/components/resume.components/Resume.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Experience from './components/Experience';
+// import Experience from './components/Experience';
 import Objective from './components/Objective';
 import Profile from './components/profile.components/Profile';
+import WorkExperience from './components/experience.components/WorkExperience';
 import CVProjects from './components/projects.components/CVProjects';
+
 
 import './resume.styles.css';
 
@@ -15,7 +17,8 @@ const Resume = () => {
             </div>
             <div className="rightSection">
                 <Objective/>
-                <Experience/>
+                {/* <Experience/> */}
+                <WorkExperience/>
                 <CVProjects/>
             </div>
         </div>
