3aa95d68800b8eb3daba6e440dd810a1f893fef3
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-17 00:45:49 +0200

    cvProjects section added

--- src/components/resume.components/Resume.js
+++ src/components/resume.components/Resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Experience from './components/Experience';
 import Objective from './components/Objective';
 import Profile from './components/profile.components/Profile';
+import CVProjects from './components/projects.components/CVProjects';
 
 import './resume.styles.css';
 
@@ -15,6 +16,7 @@ const Resume = () => {
             <div className="rightSection">
                 <Objective/>
                 <Experience/>
+                <CVProjects/>
             </div>
         </div>
     )
