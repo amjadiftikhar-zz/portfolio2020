82132a10ebe57eed4a93a4379b87aad1a5bd7f4f
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-18 14:33:59 +0200

    CertificateCard component added

--- src/components/resume.components/components/certificates.components/Certificates.js
+++ src/components/resume.components/components/certificates.components/Certificates.js
@@ -1,11 +1,31 @@
+import { index } from 'd3';
 import React from 'react';
+import CertificateCard from './CertificateCard';
+import './certificates.styles.css';
+
+const certificateData = [
+    {
+        duration: 'Microsoft Certified Professional, MCP ID: 8074395',
+        description: 'MCSE, MCSA, MCP and MCTS (windows 2007)'
+    }    
+]
 
 const Certificates = () => {
 
     return (
-        <div>
+        <div className='certificateSection'>
             <h3 className='cvCertificateTitle'>Certificates</h3>
-            
+            <div className='certificateContainer'>
+                {certificateData.map((c, index) => {
+                    return(
+                        <CertificateCard                  
+                            key={index}
+                            contentDuration={c.duration}
+                            contentDescription={c.description}
+                        />
+                    )
+                })} 
+            </div>           
         </div>
     )
 }
