9024a42e56fc805f6b3a0e00390262cf8a25d878
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-18 12:07:57 +0200

    certificate.components created

---
+++ src/components/resume.components/components/certificates.components/Certificates.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Certificates = () => {
+
+    return (
+        <div>
+            <h3 className='cvCertificateTitle'>Certificates</h3>
+            
+        </div>
+    )
+}
+
+export default Certificates;
