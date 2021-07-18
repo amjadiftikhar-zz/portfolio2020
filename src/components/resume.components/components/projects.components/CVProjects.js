9204d87bcb7e0ad0823b28906d313bdb87f01a8d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-18 12:09:36 +0200

    changes made

--- src/components/resume.components/components/projects.components/CVProjects.js
+++ src/components/resume.components/components/projects.components/CVProjects.js
@@ -2,12 +2,29 @@ import React from 'react'
 import CVProjectCard from './CVProjectCard';
 import "./cvProjects.styles.css";
 
+const projectData = [
+    {
+        duration: 'Movie Catalogue (10/2020 - 10/2020)',
+        description: 'Application built in React using Star Wars API to show movies and movie characters as well as character specs. Tool used are, Javascript, React, HTML, CSS and API, Git, Github.'
+    }    
+]
+
 const CVProjects = () => {
 
     return (
         <div className="cvProjectContainer">
             <h3 className='cvProjectTitle'>Projects</h3>
-            <CVProjectCard/>
+            {
+                projectData.map((p, index) => {
+                    return(
+                        <CVProjectCard
+                            key={index}
+                            contentDuration={p.duration}
+                            contentDescription={p.description}
+                        />
+                    )
+                })
+            }            
         </div>
     )
 }
