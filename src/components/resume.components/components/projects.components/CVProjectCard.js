b2b8c079644159f5e6eab2fdeb94414fb649adb3
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-18 12:09:03 +0200

    changes made

--- src/components/resume.components/components/projects.components/CVProjectCard.js
+++ src/components/resume.components/components/projects.components/CVProjectCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './cvProjects.styles.css';
 
-const CVProjectCard = () => {
+const CVProjectCard = (props) => {
 
     return (
         <div className="cvProjectCard"> 
-            <span className="cvProjectName">Movie Catalogue (10/2020 - 10/2020)</span>
+            <span className="cvProjectName">
+                {/* Movie Catalogue (10/2020 - 10/2020) */}
+                {props.contentDuration}
+            </span>
             <ul className="cvProjectDescription">
                 <li>
-                    Application built in React using Star Wars API to show movies and
+                    {/* Application built in React using Star Wars API to show movies and
                     movie characters as well as character specs. Tool used are, Javascript, 
-                    React, HTML, CSS and API, Git, Github. 
+                    React, HTML, CSS and API, Git, Github.  */}
+                    {props.contentDescription}
                 </li>
             </ul>            
         </div>
