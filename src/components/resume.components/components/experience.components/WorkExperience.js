a65eb109c035255b358654435ad83ad59eb4fdb8
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-20 14:23:39 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -4,11 +4,11 @@ import ExperienceCard from './ExperienceCard';
 import TaskDescription from './TaskDescription';
 import './workExperience.styles.css';
 
-const experienceData = [
-    {
+export const experienceData = [
+    { 
         jobTitle:'Frontend Developer',
         companyName: 'Molecular Quantum Solutions',
-        duration: '01/01/2021 - Present',
+        duration: '01/2021 - Present',
         task: 'Tasks:',
         companyLocation: 'Søborg, Denmark',
         taskDetails: [
@@ -17,7 +17,21 @@ const experienceData = [
             'Developing design and collaboration with the design team',
             'Making reusable components and writing test using React testing library'
         ]
-    }
+    },
+    {
+        jobTitle:'Frontend Developer',
+        companyName: 'Unifounder',
+        duration: '11/2020 - 01/2021',
+        task: 'Tasks:',
+        companyLocation: 'Frederiksberg, Denmark',
+        taskDetails: [
+            'Responsible for developing the design also help designing the platform',
+            'Implementation from concept through deployment, with a responsive, mobile first approach',
+            'Refactoring and decomposing exiting code and structure',
+            'Perform code reviews, coordinating with designers and development team',
+            'Diagnosing, troubleshooting and resolving technical issues'
+        ],
+    },
 ]
 
 const WorkExperience = () => {
@@ -33,21 +47,33 @@ const WorkExperience = () => {
                             jobTitle={e.jobTitle}
                             companyName={e.companyName}
                             duration={e.duration}
-                            task={e.task}
-                            companyLocation={e.companyLocation}                            
-                        />
-                    )
+                            companyLocation={e.companyLocation} 
+                            task={e.task}                           
+                        /> 
+                        
+                        // e.taskDetails.map((et, index) => {
+                        //     return(
+                        //         <TaskDescription
+                        //             taskDescription={et}
+                        //         />
+                        //     )
+                        // })
+                    ) 
+                    
                 })
             }
             {
-                // console.log('hello', experienceData[0].taskDetails)
-            experienceData[0].taskDetails.map((et, index) => {
-                return(
-                    <TaskDescription
-                        taskDescription={et}
-                    />
-                )
-            })
+                // experienceData.map((e, index) => {
+                //     return(
+                //         e.taskDetails.map((et, index) => {
+                //             return(
+                //                 <TaskDescription
+                //                     taskDescription={et}
+                //                 />
+                //             )
+                //         })                        
+                //     )
+                // })
             }            
             
         </div>
