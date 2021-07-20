0a2341fb78f798f2cc0178e73840cf2f7fa33c07
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-21 00:10:52 +0200

    Education and VolunteerWork components added

--- src/components/resume.components/Resume.js
+++ src/components/resume.components/Resume.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import Experience from './components/Experience';
 import Objective from './components/Objective';
 import Profile from './components/profile.components/Profile';
 import WorkExperience from './components/experience.components/WorkExperience';
 import CVProjects from './components/projects.components/CVProjects';
-
-
-import './resume.styles.css';
 import Skills from './components/skills.components/Skills';
 import Certificates from './components/certificates.components/Certificates';
 import Languages from './components/language.components/Languages';
 import References from './components/references.components/References';
+import VolunteerExperience from './components/experience.components/VolunteerExperience';
+import EducationExperience from './components/experience.components/EducationExperience';
+
+import './resume.styles.css';
 
 const Resume = () => {
 
@@ -25,9 +25,10 @@ const Resume = () => {
             </div>
             <div className="rightSection">
                 <Objective/>
-                {/* <Experience/> */}
                 <WorkExperience/>
+                <VolunteerExperience/>
                 <CVProjects/>
+                <EducationExperience/>
             </div>
         </div>
     )
