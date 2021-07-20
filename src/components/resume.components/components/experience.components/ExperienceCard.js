1b713457a172f14cb92024c0978472d09753c5c8
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-21 00:10:19 +0200

    changes made

--- src/components/resume.components/components/experience.components/ExperienceCard.js
+++ src/components/resume.components/components/experience.components/ExperienceCard.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import TaskDescription from './TaskDescription';
-import {experienceData} from './WorkExperience';
+// import {experienceData} from './WorkExperience';
 import './workExperience.styles.css';
 
+// const taskDetailsData = [ 
+//     [
+//         'Responsible for the frontend tasks related to building a platform that hosts different tools for R & D',
+//         ' Planning and setting up frontend architecture, diagnosing, troubleshooting and resolving technical issues',
+//         'Developing design and collaboration with the design team',
+//         'Making reusable components and writing test using React testing library'
+//     ],
+//     [
+//         'Responsible for developing the design also help designing the platform',
+//         'Implementation from concept through deployment, with a responsive, mobile first approach',
+//         'Refactoring and decomposing exiting code and structure',
+//         'Perform code reviews, coordinating with designers and development team',
+//         'Diagnosing, troubleshooting and resolving technical issues'
+//     ],
+// ]
+
 const ExperienceCard = (props) => {
+
+    // console.log()
 
     return (
         <div className='experienceMainCard'>
@@ -24,7 +42,7 @@ const ExperienceCard = (props) => {
                 </div>
                 <div className="taskDetails">
                     <ul className="taskDescription">
-                        {experienceData[0].taskDetails.map((td, index) => {
+                        {props.data.map((td, index) => {
                             return(
                                 <TaskDescription
                                     key={index}
