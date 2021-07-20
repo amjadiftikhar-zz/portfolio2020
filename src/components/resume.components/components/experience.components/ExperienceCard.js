aa19bf3418a6f6859f6bbf33ba126942df5a4eba
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-20 12:30:16 +0200

    props passed and took dynamic approach

--- src/components/resume.components/components/experience.components/ExperienceCard.js
+++ src/components/resume.components/components/experience.components/ExperienceCard.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './workExperience.styles.css';
 
-const ExperienceCard = () => {
+const ExperienceCard = (props) => {
 
     return (
         <div className='experienceMainCard'>
-            <h4 className='jobTitle'>Frontend Developer</h4>
-            <h2 className='companyNameTitle'>Molecular Quantum Solutions</h2>
+            {/* <h4 className='jobTitle'>Frontend Developer</h4> */}
+            <h4 className='jobTitle'> {props.jobTitle} </h4>
+            {/* <h2 className='companyNameTitle'>Molecular Quantum Solutions</h2> */}
+            <h2 className='companyNameTitle'> {props.companyName} </h2>
             <div className="jobDetailSection">
                 <div className='jobDetails'>
                     <div className='timePeriod'>
-                        <span className='details'>01/01/2021 - Present</span>
-                        <span className='details'>Tasks:</span>
+                        {/* <span className='details'>01/01/2021 - Present</span> */}
+                        <span className='details'> {props.duration} </span>
+                        {/* <span className='details'>Tasks:</span> */}
+                        <span className='details'> {props.task} </span>
                     </div>
-                    <span className='details'>Søborg, Denmark</span>
+                    {/* <span className='details'>Søborg, Denmark</span> */}
+                    <span className='details'> {props.companyLocation} </span>
                 </div>
-                <div className="taskDetails">
+                {/* <div className="taskDetails">
                     <ul className="taskDescription">
                         <li>
                             Responsible for the frontend tasks related to building a 
-                            platform that hosts different tools for R&D
+                            platform that hosts different tools for R & D
                         </li>
                         <li>
                             Planning and setting up frontend architecture, diagnosing, 
@@ -32,7 +37,7 @@ const ExperienceCard = () => {
                             Making reusable components and writing test using React testing library
                         </li>
                     </ul>
-                </div>
+                </div> */}
             </div>
         </div>
     )
