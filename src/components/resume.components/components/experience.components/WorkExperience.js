d61d17e0ecf333e28961be5d8b7436396de6d82b
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-20 12:31:04 +0200

    components added and object created

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -1,13 +1,55 @@
+import { index } from 'd3';
 import React from 'react';
 import ExperienceCard from './ExperienceCard';
+import TaskDescription from './TaskDescription';
 import './workExperience.styles.css';
+
+const experienceData = [
+    {
+        jobTitle:'Frontend Developer',
+        companyName: 'Molecular Quantum Solutions',
+        duration: '01/01/2021 - Present',
+        task: 'Tasks:',
+        companyLocation: 'Søborg, Denmark',
+        taskDetails: [
+            'Responsible for the frontend tasks related to building a platform that hosts different tools for R & D',
+            ' Planning and setting up frontend architecture, diagnosing, troubleshooting and        resolving technical issues',
+            'Developing design and collaboration with the design team',
+            'Making reusable components and writing test using React testing library'
+        ]
+    }
+]
 
 const WorkExperience = () => {
 
     return (
         <div className="workExperienceContainer">
             <h3 className='experienceTitle'>Work Experience</h3>
-            <ExperienceCard/>
+            {
+                experienceData.map((e, index) => {
+                    return(
+                        <ExperienceCard
+                            key={index}
+                            jobTitle={e.jobTitle}
+                            companyName={e.companyName}
+                            duration={e.duration}
+                            task={e.task}
+                            companyLocation={e.companyLocation}                            
+                        />
+                    )
+                })
+            }
+            {
+                // console.log('hello', experienceData[0].taskDetails)
+            experienceData[0].taskDetails.map((et, index) => {
+                return(
+                    <TaskDescription
+                        taskDescription={et}
+                    />
+                )
+            })
+            }            
+            
         </div>
     )
 }
