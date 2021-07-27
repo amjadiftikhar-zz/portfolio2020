ba68ead57129eea7aa96059bf70f0d4e5d805c23
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-28 00:43:19 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -2,14 +2,15 @@ import React from 'react';
 // import Title1 from './titles.components/Title1';
 import './objective.styles.css';
 import ObjectiveCard from './ObjectiveCard';
+import objectiveData from '../../../../assets/resume_eng.json';
 
-const objectiveData = [
-    {
-        name: 'Amjad Iftikhar',
-        jobTitle: 'Frontend Developer',
-        objective: 'Curious and talented developer with a flair for creating elegant solutions in the least amount of time and have passion for responsive website design as well as a firm believer in the mobile-first approach. Have contributed in different projects to learn new development techniques and very passionate about new fields in the domain of tech and.'
-    }
-]
+// const objectiveData = [
+//     {
+//         name: 'Amjad Iftikhar',
+//         jobTitle: 'Frontend Developer',
+//         objective: 'Curious and talented developer with a flair for creating elegant solutions in the least amount of time and have passion for responsive website design as well as a firm believer in the mobile-first approach. Have contributed in different projects to learn new development techniques and very passionate about new fields in the domain of tech and.'
+//     }
+// ]
 
 
 const Objective = () => {
@@ -17,7 +18,7 @@ const Objective = () => {
     return (
         <div className="objectiveSection">
             {/* <Title1 title='Amjad Iftikhar' /> */}
-            {objectiveData.map((o, index) => {
+            {objectiveData.objective.map((o, index) => {
                 return(
                     <ObjectiveCard
                         key={index}
