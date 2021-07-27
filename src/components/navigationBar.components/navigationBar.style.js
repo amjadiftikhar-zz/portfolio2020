fe0382a53d8f5ebfb5c447343b569ad547ac77d3
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-28 00:40:37 +0200

    changes made

--- src/components/navigationBar.components/navigationBar.style.js
+++ src/components/navigationBar.components/navigationBar.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const NavigationStyles = styled.div`
   .navbar {
+    // width: 100%;
     // padding: 1.3em;
     padding: 1em;
     margin: 2%;
@@ -13,9 +14,17 @@ const NavigationStyles = styled.div`
     &:hover {
       // color: red;
       color: orange;
-      background-color: #C1BDB5;
+      // background-color: #C1BDB5;
       font-weight: 'bold';
     }
+  }
+  .active {
+    background-color: #060655!important;
+    color: white !important;
+    border-bottom: 2px solid white !important;
+  }
+  .active:hover {
+    color: orange !important;
   }
   .toggle {
     font-size: 1rem;
@@ -25,6 +34,13 @@ const NavigationStyles = styled.div`
     margin-left: 0.2em;
     font-size: 0.85rem;
   }
+
+  /* Media query for devices upto 414px screens */
+    @media only screen and (max-width: 414px) {
+      .navbar {
+        // width: 100%;
+      }
+    }
 `;
 
 export default NavigationStyles;
