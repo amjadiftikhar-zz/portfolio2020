2034b3a17661963eb7457c13089b2d18c97d4440
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-29 00:04:29 +0200

    changes made

--- src/components/resume.components/components/experience.components/EducationExperience.js
+++ src/components/resume.components/components/experience.components/EducationExperience.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import ExperienceCard from './ExperienceCard';
 import './workExperience.styles.css';
+import educationData from '../../../../assets/resume_eng.json';
 
-export const educationData = [
-    { 
-        jobTitle:'Full Stack Developer Program',
-        companyName: 'Hack Your Future-Copenhagen',
-        duration: '08/2019 - 04/2020',
-        task: 'Courses:',
-        companyLocation: 'Copenhagen, Denmark',
-        taskDetails: [
-            'Javascript, React, Nodejs, HTML, CSS, mySql',
-        ]
-    },
-    {
-        jobTitle:'Masters in Computer Systems Engineering',
-        companyName: 'Halmstad University',
-        duration: '01/2008 - 03/2010',
-        task: 'Courses:',
-        companyLocation: 'Halmstad, Sweden',
-        taskDetails: [
-            'Advanced Routing, Multilayer Switching'
-        ],
-    },
-    {
-        jobTitle:"Bachelor's in Computer Science",
-        companyName: 'AIOU University Islamabad',
-        duration: '09/2000 - 03/2005',
-        task: 'Courses:',
-        companyLocation: 'Islamabad, Pakistan',
-        taskDetails: [
-            'C++, Java, Visual C++, Oracle SQL, VBasic'
-        ],
-    }
-]
+
+// export const educationData = [
+//     { 
+//         jobTitle:'Full Stack Developer Program',
+//         companyName: 'Hack Your Future-Copenhagen',
+//         duration: '08/2019 - 04/2020',
+//         task: 'Courses:',
+//         companyLocation: 'Copenhagen, Denmark',
+//         taskDetails: [
+//             'Javascript, React, Nodejs, HTML, CSS, mySql',
+//         ]
+//     },
+//     {
+//         jobTitle:'Masters in Computer Systems Engineering',
+//         companyName: 'Halmstad University',
+//         duration: '01/2008 - 03/2010',
+//         task: 'Courses:',
+//         companyLocation: 'Halmstad, Sweden',
+//         taskDetails: [
+//             'Advanced Routing, Multilayer Switching'
+//         ],
+//     },
+//     {
+//         jobTitle:"Bachelor's in Computer Science",
+//         companyName: 'AIOU University Islamabad',
+//         duration: '09/2000 - 03/2005',
+//         task: 'Courses:',
+//         companyLocation: 'Islamabad, Pakistan',
+//         taskDetails: [
+//             'C++, Java, Visual C++, Oracle SQL, VBasic'
+//         ],
+//     }
+// ]
 
 const EducationExperience = () => {
 
@@ -41,7 +43,7 @@ const EducationExperience = () => {
         <div className="workExperienceContainer">
             <h3 className='experienceTitle'>Education</h3>
             {
-               educationData.map((e, index) => {
+                educationData.education.map((e, index) => {
                     return(
                         <ExperienceCard
                             key={index}
