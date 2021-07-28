84aa43a9d81c86a292d4c65e299bd9648128094c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-29 00:04:56 +0200

    changes made

--- src/components/resume.components/components/certificates.components/Certificates.js
+++ src/components/resume.components/components/certificates.components/Certificates.js
@@ -2,16 +2,19 @@ import React from 'react';
 import CertificateCard from './CertificateCard';
 import './certificates.styles.css';
 
-const certificateData = [
-    {
-        duration: 'Microsoft Certified Professional, MCP ID: 8074395',
-        description: 'MCSE, MCSA, MCP and MCTS (windows 2007)'
-    },
-    {
-        duration: 'Cisco Academic Certifications (2008)',
-        description: 'CCNP (Cisco Certified Network Professional) & CCNA (Cisco Certified Network Associate)'
-    }        
-]
+import certificatesData from '../../../../assets/resume_eng.json';
+
+
+// const certificateData = [
+//     {
+//         duration: 'Microsoft Certified Professional, MCP ID: 8074395',
+//         description: 'MCSE, MCSA, MCP and MCTS (windows 2007)'
+//     },
+//     {
+//         duration: 'Cisco Academic Certifications (2008)',
+//         description: 'CCNP (Cisco Certified Network Professional) & CCNA (Cisco Certified Network Associate)'
+//     }        
+// ]
 
 const Certificates = () => {
 
@@ -19,7 +22,7 @@ const Certificates = () => {
         <div className='certificateSection'>
             <h3 className='cvCertificateTitle'>Certificates</h3>
             <div className='certificateContainer'>
-                {certificateData.map((c, index) => {
+                {certificatesData.certificates.map((c, index) => {
                     return(
                         <CertificateCard                  
                             key={index}
