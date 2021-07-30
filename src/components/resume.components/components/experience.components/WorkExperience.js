43ca0c3f38ed3e2e9ff5b04908cedc3a324cb7e6
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-31 00:11:57 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ExperienceCard from './ExperienceCard';
 import TaskDescription from './TaskDescription';
 import './workExperience.styles.css';
-import experienceData from '../../../../assets/resume_eng.json';
+// import experienceData from '../../../../assets/resume_eng.json';
+import experienceData from '../../../../assets/resume_dk.json';
+
 
 
 // export const experienceData = [
