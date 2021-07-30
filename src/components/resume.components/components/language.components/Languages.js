2763d80c393a1c399734ef1b1ccec0b60bc343c9
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-07-31 00:13:14 +0200

    changes made

--- src/components/resume.components/components/language.components/Languages.js
+++ src/components/resume.components/components/language.components/Languages.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './language.styles.css';
 import LanguageCard from './LanguageCard';
 
-import languageData from '../../../../assets/resume_eng.json';
+// import languageData from '../../../../assets/resume_eng.json';
+import languageData from '../../../../assets/resume_dk.json';
+
 
 
 // const languageData = [
