c609c955b629f7dcc585c201ec35b6cdd070ede3
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-03 21:53:06 +0200

    changes made

--- src/components/resume.components/Resume.js
+++ src/components/resume.components/Resume.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 // import Objective from './components/Objective';
 import Objective from './components/objective.components/Objective';
 import Profile from './components/profile.components/Profile';
@@ -11,12 +11,15 @@ import References from './components/references.components/References';
 import VolunteerExperience from './components/experience.components/VolunteerExperience';
 import EducationExperience from './components/experience.components/EducationExperience';
 
+import '../../i18n/i18n';
+
+
 import './resume.styles.css';
 
 const Resume = () => {
 
     return (
-        <div className='resumeContainer'>
+        <div className='resumeContainer' >
             <div className="leftSection">
                 <Profile/>
                 <Skills/>
@@ -25,7 +28,9 @@ const Resume = () => {
                 <References/>
             </div>
             <div className="rightSection">
-                <Objective/>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Objective/>
+                </Suspense>
                 <WorkExperience/>
                 <VolunteerExperience/>
                 <CVProjects/>
