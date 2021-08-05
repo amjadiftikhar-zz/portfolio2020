277fce9d068abd4dc72299b2d8a7bcb7d3fb5659
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-06 00:33:42 +0200

    changes made

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './i18n/i18n';
+
+import './index.css';
 
 import "bootstrap/dist/css/bootstrap.css";
 
