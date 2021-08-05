2531136223e569856f8a6c58fcfbbdfea0635769
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-06 00:33:26 +0200

    changes made

--- src/components/resume.components/components/projects.components/CVProjects.js
+++ src/components/resume.components/components/projects.components/CVProjects.js
@@ -8,7 +8,6 @@ import projectsData from '../../../../assets/resume_eng.json';
 
 // const projectData = [
 //     {
-//         duration: 'Movie Catalogue (10/2020 - 10/2020)',
 //         description: 'Application built in React using Star Wars API to show movies and movie characters as well as character specs. Tool used are, Javascript, React, HTML, CSS and API, Git, Github.'
 //     },
 //     {
