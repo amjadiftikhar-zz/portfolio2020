761a9f6883423af3a24cf4ec837ef0518595d972
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-09 21:45:54 +0200

    changes made

--- src/components/resume.components/components/experience.components/ExperienceCard.js
+++ src/components/resume.components/components/experience.components/ExperienceCard.js
@@ -51,7 +51,7 @@ const ExperienceCard = (props) => {
                             )
                         })}
                         {/* <li>
-                            platform that hosts different tools for R & D
+                            platform tsts different tools for R & D
                         </li>
                         <li>
                             Planning and setting up frontend architecture, diagnosing, 
