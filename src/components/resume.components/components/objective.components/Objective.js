15d7d83a60207e1e681fd2bc426987541dde88da
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-15 20:06:31 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // import Title1 from './titles.components/Title1';
 import './objective.styles.css';
 import ObjectiveCard from './ObjectiveCard';
-import objectiveData from '../../../../assets/resume_eng.json';
+// import objectiveData from '../../../../assets/resume_eng.json';
 // import objectiveData from '../../../../assets/resume_dk.json';
 
-// import axios from 'axios';
+import axios from 'axios';
 
 
 import { useTranslation, Trans, withTranslation } from 'react-i18next';
@@ -13,13 +13,28 @@ import { useTranslation, Trans, withTranslation } from 'react-i18next';
 
 const Objective = () => {
 
+    const [objective, setObjective] = useState(null)
+
+    useEffect(() => {
+        // fetch('http://localhost:9000/objective')
+        // .then(response => response.json())
+        // .then(data =>  setObjective(data))
+        axios.get('http://localhost:9000/objective')
+        .then(response => setObjective(response.data))
+    }, [])
+
+    console.log('hello obj', objective)
+
+    
+
     const {t, i18n} = useTranslation();
 
     return (
         <div className="objectiveSection">
             {/* <h2>{t('danish.hello')}</h2> */}
             {/* <Trans t={t}> */}
-                { objectiveData.objective.map((o, index) => {
+                {/* { objectiveData.objective.map((o, index) => { */}
+                {objective && objective.map((o, index) => {
                     return(
                         <ObjectiveCard
                             key={index}
