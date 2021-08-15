077b00bf6d3995d1f3f89e0c850092ff9d544cdc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-16 00:16:06 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -10,7 +10,7 @@ import experienceData from '../../../../assets/resume_eng.json';
 // export const experienceData = [
 //     { 
 //         jobTitle:ontend Deeloper',
-//         companyName: 'Moeular Quantum Solutions',
+//         companyName: 'Moular Quantum Solutions',
 //         duration: '01/2021 - Present',
 //         task: 'Tasks:',
 //         taskDetails: [
