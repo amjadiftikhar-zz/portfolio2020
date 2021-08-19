7fecdb46fb688665d44e450009d2e94fecd2f3bd
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-20 00:15:04 +0200

    changes made

--- src/components/resume.components/components/certificates.components/Certificates.js
+++ src/components/resume.components/components/certificates.components/Certificates.js
@@ -9,7 +9,7 @@ import certificatesData from '../../../../assets/resume_eng.json';
 // const certificateData = [
 //     },
 //     {
-//         description: 'CCNP (Cis Cerd 'work Professional) & CCNA (Cisco Certified Network Associate)'
+//         description: 'CCNP (Ci Cerd 'work Professional) & CCNA (Cisco Certified Network Associate)'
 //     }        
 // ]
 
