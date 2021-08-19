d2259467d4bf29dfc5f236734c462ed59e21362f
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-20 00:12:07 +0200

    changes made

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import axios from 'axios';
+
 import './i18n/i18n';
 
 import './index.css';
 
 import "bootstrap/dist/css/bootstrap.css";
 
+axios.defaults.headers.common['Accept-Language'] = localStorage.getItem('lang') || 'en';
+
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    {/* <Suspense fallback={<div>Loading...</div>}> */}
       <App />
-    </Suspense>
+    {/* </Suspense> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
