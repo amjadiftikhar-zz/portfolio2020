11b6991814655b14fb1adfbecd781727be377e70
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-22 10:16:43 +0200

    changes made

--- src/components/resume.components/components/references.components/References.js
+++ src/components/resume.components/components/references.components/References.js
@@ -11,7 +11,7 @@ import './references.styles.css';
 //         email: 'mark@mqs.dk '
 //     },
 //     {
-//         reference: ' Ismaili t Dwarf)',
+//         reference:  Ismaili t Dwarf)',
 //         email: 'ismailirahim6@gmail.com'
 //     },       
 // ]
