0448c07d8eb36805371d85c847f4bd12149f8b91
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-23 18:02:53 +0200

    changes made

--- src/components/resume.components/components/projects.components/CVProjects.js
+++ src/components/resume.components/components/projects.components/CVProjects.js
@@ -10,7 +10,7 @@ import projectsData from '../../../../assets/resume_eng.json';
 //     {
 //     },
 //     {
-//         description: 's a startuspecilizin  vertical farming with robotics. A dashboard application (https://fp-class11.herokuapp.com/dashboard) for Seasony and tools used are, JavaScript, NodeJS, React, MySql, Knex.js, Storybook, Express, Swagger, HTML and'
+//         description: 's a startuspecilizi  vertical farming with robotics. A dashboard application (https://fp-class11.herokuapp.com/dashboard) for Seasony and tools used are, JavaScript, NodeJS, React, MySql, Knex.js, Storybook, Express, Swagger, HTML and'
 //     },
 //     {
 //         duration: 'Meal Sharing Applicatin (02/2020 - 02/2020)',
