cda29a862ac11924eff8220227d7fea550a0ad3d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-28 00:14:09 +0200

    changes made

--- src/components/resume.components/components/experience.components/TaskDescription.js
+++ src/components/resume.components/components/experience.components/TaskDescription.js
@@ -8,7 +8,7 @@ const TaskDescription = (props) => {
             {/* <ul className="taskDescription"> */}
                 <li>{props.taskDescription}</li>
                 {/* <li>
-                    Responthe frontend tasks related to building a 
+                    Respothe frontend tasks related to building a 
                     platform that hosts different tools for R & D
                 </li>
                 <li>
