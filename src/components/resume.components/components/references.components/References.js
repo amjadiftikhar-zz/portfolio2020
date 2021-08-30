3fc6d1b027dfbfc383e3af05b1f8f94f0b7cc549
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-30 22:42:08 +0200

    changes made

--- src/components/resume.components/components/references.components/References.js
+++ src/components/resume.components/components/references.components/References.js
@@ -12,7 +12,7 @@ import './references.styles.css';
 //     },
 //     {
 //         refe Ismaili t Dwarf)',
-//         email: 'ismailirahim6@gmacom'
+//         email: 'ismailirahim6@gmcom'
 //     },       
 // ]
 
