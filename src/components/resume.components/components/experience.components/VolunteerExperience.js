d47a9e348ac11f47a935852a86ff32bc8f0cd046
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-08-31 23:53:08 +0200

    changes made

--- src/components/resume.components/components/experience.components/VolunteerExperience.js
+++ src/components/resume.components/components/experience.components/VolunteerExperience.js
@@ -13,7 +13,7 @@ import volunteerExperienceData from '../../../../assets/resume_eng.json';
 //         companyLocatio Denmark',
 //         taskDetails: [
 //             'A signle page application, developed in React',
-//             'I succssfully completed igot a deep insight of working with React.js from scratch to deployment',
+//             'I succssfully completed got a deep insight of working with React.js from scratch to deployment',
 //             'Made reusable components and wrote tests using React testing library',
 //         ]
 //     },
