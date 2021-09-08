8ed01fcd440e28e3515d26bcbad04cc685421d02
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-09-08 23:34:59 +0200

    change made

--- src/components/resume.components/components/objective.components/ObjectiveCard.js
+++ src/components/resume.components/components/objective.components/ObjectiveCard.js
@@ -1,4 +1,31 @@
 import React from 'react';
+
+const ObjectiveCard = (props) => {
+
+    return (
+        <div className="objectiveCard">
+            <h1> {props.myName} </h1>
+            <h3> {props.jobTitle} </h3>
+            <p>
+               {props.objective}
+            </p>
+        </div>
+    );
+}
+
+export default ObjectiveCard;
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
 import './objective.styles.css';
 
 import { useTranslation } from 'react-i18next';
