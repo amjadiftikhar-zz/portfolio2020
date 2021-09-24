81195d40cc9f7bbbc7d7f66ea3a3d1ae82b1ac5b
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-09-24 22:03:53 +0200

    changes made

--- src/components/resume.components/components/experience.components/TaskDescription.js
+++ src/components/resume.components/components/experience.components/TaskDescription.js
@@ -9,7 +9,7 @@ const TaskDescription = (props) => {
                 <li>{props.taskDescription}</li>
                 {/* <li>
                     uilding a 
-                    platform that hosts different tools for R & D
+                    platform that hosts diferent tools for R & D
                 </li>
                 <li>
                     Planning and seng up frontend architecture, diagnosing, 
