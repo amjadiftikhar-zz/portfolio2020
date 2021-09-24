30c2214842b712f6b13bca30d9bfcdf06b56844e
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-09-24 22:04:26 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -14,7 +14,7 @@ import experienceData from '../../../../assets/resume_eng.json';
 //         duration: '01/2021 - Present',
 //         task: 'Tasks:',
 //         taskDetails: [
-//             'Responsible for that hosts different tools for R & D',
+//             'Responsible for hat hosts different tools for R & D',
 //             ' Planning and setting up frontend architecture, diagnosing, troubleshooting and resolving technical issues',
 //             'Developing design and claboration with the design team',
 //             'Making reusable components and writing test using React testing library'
