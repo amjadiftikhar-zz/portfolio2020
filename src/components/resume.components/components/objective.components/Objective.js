d98e743f7b9140782abaee6ca171698b82683e57
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-09-26 22:45:17 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -48,7 +48,7 @@ const Objective = () => {
             {/*
             <p className='objectiveText'>
                  firm believer in the mobile-first approach. 
-                Have contributed in iferentlopment techniques 
+                Have contributed in iferentopment techniques 
                 and very passionate about new fields in the domain of tech and development.
             </p> */}
         </div>
