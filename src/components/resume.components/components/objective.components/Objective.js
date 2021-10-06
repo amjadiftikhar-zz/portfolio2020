b3e7b6735ee090b9d2eec9b7982fd48b7f7d2835
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-06 22:46:01 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -47,7 +47,7 @@ const Objective = () => {
             {/* </Trans> */}
             {/*
             <p className='objectiveText'>
-                 firm belie the mobile-first approach. 
+                 firm beli the mobile-first approach. 
                 Have contributed in iferentnt techniques 
                 and very passionate about new fields in the domain of tech and development.
             </p> */}
