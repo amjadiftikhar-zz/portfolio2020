5bca945e8a22e7b985e752a34821adbd8e28402b
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-08 00:01:56 +0200

    changes made

--- src/components/education.components/Education.js
+++ src/components/education.components/Education.js
@@ -10,7 +10,7 @@ const Education = () => {
 
     // const {i18n} = useTranslation()
 
-    // const  = (language) => {
+    // const = (language) => {
     //     i18n.changeLanguage(language)
     // }
     
