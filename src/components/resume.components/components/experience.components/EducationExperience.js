70f7adf4e305e146ba9847fc7ebf2ab45b4a7eaa
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-11 22:48:01 +0200

    changes made

--- src/components/resume.components/components/experience.components/EducationExperience.js
+++ src/components/resume.components/components/experience.components/EducationExperience.js
@@ -9,7 +9,7 @@ import './workExperience.styles.css';
 
 // export const educationData = [
 //     {         jobTitle:'Full Stac Developer Program',
-//                  - 4/2020',
+//                 - 4/2020',
 //         task: 'Courses:',
 //         taskDetails: [
 //         ]
