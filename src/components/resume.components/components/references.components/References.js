f5cf2e577a1502b4c87b59ef837154b8b02aa00a
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-19 00:25:09 +0200

    changes made

--- src/components/resume.components/components/references.components/References.js
+++ src/components/resume.components/components/references.components/References.js
@@ -11,7 +11,7 @@ import './references.styles.css';
 //         email: 'mark@mqs.dk '
 //     },
 //     {
-//         refe Ismaili t        
+//         refe Ismaili t       
 // ]
 
 const References = () => {
