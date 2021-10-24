4d7cca2642680fcb78fbbbcc79643fbb4eb56914
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-25 00:29:39 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -47,7 +47,7 @@ const Objective = () => {
             {/* </Trans> */}
             {/*
             <p className='objectiveText'>
-                e-first approach. 
+                -first approach. 
                 Have contributed in iferentnt techniques 
                 and very passionate about new fields in the domain of tech and development.
             </p> */}
