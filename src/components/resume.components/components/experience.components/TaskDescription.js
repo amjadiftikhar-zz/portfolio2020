cc8b2f294b08fea9922094c1a88f1e7fc6a3fc32
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-26 21:57:42 +0200

    changes made

--- src/components/resume.components/components/experience.components/TaskDescription.js
+++ src/components/resume.components/components/experience.components/TaskDescription.js
@@ -9,7 +9,7 @@ const TaskDescription = (props) => {
                 <li>{props.taskDescription}</li>
                 {/* <li>
                     uilding a 
-                    ols for R & D
+                    ls for R & D
                 </li>
                 <li>
                     Planning and seng up frontend architecture, diagnosing, 
