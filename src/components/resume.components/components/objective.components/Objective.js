c7a27bba620e6033d1ad328738a802b7a8184df9
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-29 00:06:42 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -47,7 +47,7 @@ const Objective = () => {
             {/* </Trans> */}
             {/*
             <p className='objectiveText'>
-                st approach. 
+                t approach. 
                 Have contributed in iferentnt techniques 
                 and very passionate about new fields in the domain of tech and development.
             </p> */}
