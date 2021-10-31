7c0c0f83f6899a571ae026a8eb35d8b62abd4807
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-10-31 23:11:40 +0100

    changes made

--- src/components/resume.components/components/language.components/Languages.js
+++ src/components/resume.components/components/language.components/Languages.js
@@ -15,7 +15,7 @@ import languageData from '../../../../assets/resume_eng.json';
 //     }, 
 //     {
 //         language: 'Urdu',
-//         level: 'Native or Bilingual Pro'
+//         level: 'Native or Bilingual Pr'
 //     }        
 // ]
 
