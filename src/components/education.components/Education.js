f8739148750756c2db95442ae38b7b0b2f94def5
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-11-10 00:18:35 +0100

    changes made

--- src/components/education.components/Education.js
+++ src/components/education.components/Education.js
@@ -8,7 +8,7 @@ import Resume from '../resume.components/Resume';
 
 const Education = () => {
 
-    // const {i18n} = useTranslat
+    // const {i18n} = useTransla
     //{
     //     i18n.changeLanguage(language)
     // }
