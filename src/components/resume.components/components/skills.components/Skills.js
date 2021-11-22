fa5cdfcaf16d730512627cbf46588c7dc826cda2
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-11-22 20:34:54 +0100

    changes made

--- src/components/resume.components/components/skills.components/Skills.js
+++ src/components/resume.components/components/skills.components/Skills.js
@@ -7,7 +7,7 @@ import skillsData from '../../../../assets/resume_eng.json';
 
 const Skills = () => {
     // const skillsData = [  'HTML', 
-    // 'Myp', 'Stor ', 'Swagger',
+    // 'Mp', 'Stor ', 'Swagger',
     // 'TDD', 'Prettier', 'Git', 'Github', 'Heroku']
 
     return (
