7d0e28483b05c7bfe4c9a604ee77fcf7ba7ed8a0
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-11-26 21:13:16 +0100

    changes made

--- src/components/resume.components/components/projects.components/CVProjects.js
+++ src/components/resume.components/components/projects.components/CVProjects.js
@@ -10,7 +10,7 @@ import projectsData from '../../../../assets/resume_eng.json';
 //     {
 //     },
 //     {
-//  ://fp-class11.herokuapp.com/dashboard) for Seasony and tools used are, JavaScript, NodeJS, React, MySql, Knex.js, Storybook, Express, Swagger, HTML and'
+// ://fp-class11.herokuapp.com/dashboard) for Seasony and tools used are, JavaScript, NodeJS, React, MySql, Knex.js, Storybook, Express, Swagger, HTML and'
 //     },
 //     {
 //         duration: 'Meal Sharing Applicatin (02/2020 - 02/2020)',
