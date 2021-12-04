e9103115595c74a7358d24b9e7d6e119f1280db4
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-12-04 21:12:55 +0100

    changes made

--- src/components/resume.components/components/language.components/Languages.js
+++ src/components/resume.components/components/language.components/Languages.js
@@ -8,7 +8,7 @@ import languageData from '../../../../assets/resume_eng.json';
 
 
 // const languageD    
-//   },
+//  },
 //guage: 'Danish',
 //         level: 'Professional Working Proficiency
 //     }, 
