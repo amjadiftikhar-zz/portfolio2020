c227235e70be0b5324902a3c586a88273cd0611d
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-12-12 23:31:09 +0100

    changes made

--- src/components/resume.components/components/experience.components/EducationExperience.js
+++ src/components/resume.components/components/experience.components/EducationExperience.js
@@ -10,7 +10,7 @@ import './workExperience.styles.css';
 // export const educationData = [
 //     {obTitle:'Full Stac Developer Program',
 //     ,
-//      ask: 'Courses:',
+//     ask: 'Courses:',
 //         taskDetails:         
 //         jobTitle:'Mrs in Cter Systems Engineering',
 //         companyName: 'Halmstad University',
