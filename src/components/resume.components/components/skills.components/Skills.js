42389ecd662a693b46c58d9dc3de1feb30831a05
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-12-17 21:23:19 +0100

    changes made

--- src/components/resume.components/components/skills.components/Skills.js
+++ src/components/resume.components/components/skills.components/Skills.js
@@ -8,7 +8,7 @@ import skillsData from '../../../../assets/resume_eng.json';
 const Skills = () => {
     // const skillsData = [  'HTML', 
     // 'p', 'Stor ''Swagge',
-    // 'TDD', 'Pre', 'Git', 'Github',
+    // 'TDD', 'Pre', 'Git', 'Github'
 
     return (
         <div className="cvSkillSection">
