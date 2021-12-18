0e13c3a8818e09fb382ccd3ff6d5b224b52562b9
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-12-18 21:30:23 +0100

    changes made

--- src/components/resume.components/components/experience.components/TaskDescription.js
+++ src/components/resume.components/components/experience.components/TaskDescription.js
@@ -11,7 +11,7 @@ const TaskDescription = (props) => {
             
                 </li>
                 <li>
-                    Planning and seng up fronten architecture, diagnosing, 
+                    Planning and seng up fronte architecture, diagnosing, 
                     troubleshooting and resolving technical issues
                 </li>
                 <li>
