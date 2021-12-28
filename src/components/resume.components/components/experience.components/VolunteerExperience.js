577f7eef8336dc4b96c9379baade55479d6485e7
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2021-12-28 23:30:02 +0100

    changes made

--- src/components/resume.components/components/experience.components/VolunteerExperience.js
+++ src/components/resume.components/components/experience.components/VolunteerExperience.js
@@ -7,7 +7,7 @@ import volunteerExperienceData from '../../../../assets/resume_eng.json';
 
 // export const volunteerExperienceData = [
 //     { 
-//ps://www.smartdeveloper.dk/',
+//s://www.smartdeveloper.dk/',
 //         duratio: '11/20 -
 //   ,
 //         taskDetails: [
