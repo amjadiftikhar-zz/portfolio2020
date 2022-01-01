a7782a5aa79b7fc8c5f07a9c3a345dcd9958fc90
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-01-02 00:01:40 +0100

    changes made

--- src/components/resume.components/components/experience.components/TaskDescription.js
+++ src/components/resume.components/components/experience.components/TaskDescription.js
@@ -11,7 +11,7 @@ const TaskDescription = (props) => {
             
                 </li>
                 <li>
-                    Planning an architecture, diagnosing, 
+                    Planning a architecture, diagnosing, 
                     troubleshooting and resolving technical issues
                 </li>
                 <li>
