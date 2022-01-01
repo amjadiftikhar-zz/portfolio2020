7e7c570c44c3c95270a513ee99e7dd00d63b0d1c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-01-02 00:01:55 +0100

    changes made

--- src/components/resume.components/components/experience.components/VolunteerExperience.js
+++ src/components/resume.components/components/experience.components/VolunteerExperience.js
@@ -7,7 +7,7 @@ import volunteerExperienceData from '../../../../assets/resume_eng.json';
 
 // export const volunteerExperienceData = [
 //     { 
-//martdeveloper.dk/',
+//artdeveloper.dk/',
 //         duratio: '11/20 -
 //   ,
 //         taskDetails: [
