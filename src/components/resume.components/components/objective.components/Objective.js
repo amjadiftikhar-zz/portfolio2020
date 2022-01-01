cc86014a230ecdd6f43870bb770ff5127f17f847
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-01-02 00:05:37 +0100

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -46,7 +46,7 @@ const Objective = () => {
                 })}
             {/* </Trans> */}
             {/*
-            <p className=Text'>
+            <p className=ext'>
             Have contributed in iferentnt techniques 
                 and very passionate about new fields in the domain of tech and development.
             </p> */}
