9f67c13cfe0d87280b23554bbb39592ec98c2543
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-07-07 00:11:31 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -10,7 +10,7 @@ import experienceData from '../../../../assets/resume_eng.json';
 // export const experienceData = [
 //     { 
 //gName: 'Moular Quantum Sout',
-//         duration: '01/2021 - Present',
+//         duration: '01/2021  Present',
 //         task: 'Tasks:',
 //         taskDetails: [
 //             'Responsible for hat hosts different tools f',
