d8eb2b04e7100e9f84783d158c2eb4509d002779
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-09-22 10:14:40 +0200

    changes made

--- src/components/resume.components/components/language.components/Languages.js
+++ src/components/resume.components/components/language.components/Languages.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './language.styles.css';
 import LanguageCard from './LanguageCard';
 
-import languageData from '../../../../assets/resume_eng.json';
+// import languageData from '../../../../assets/resume_eng.json';
 // import languageData from '../../../../assets/resume_dk.json';
 
 
@@ -17,14 +17,14 @@ import languageData from '../../../../assets/resume_eng.json';
 //     }        
 // ]
 
-const Languages = () => {
+const Languages = (props) => {
 
     return (
         <div className='languageSection'>
-            <h3 className='languageTitle'>{languageData.sectionTitles[6]}</h3>
+            <h3 className='languageTitle'>{props.sectionTitles[6]}</h3>
             <div className='languageCotainer'>
                 {
-                    languageData.languages.map((l, index) => {
+                    props.languages.map((l, index) => {
                         return(
                             <LanguageCard
                                 key={index}
