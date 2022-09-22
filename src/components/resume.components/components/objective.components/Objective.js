80948842380a701a29238255f4aa9a8883f080f2
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-09-22 10:14:17 +0200

    changes made

--- src/components/resume.components/components/objective.components/Objective.js
+++ src/components/resume.components/components/objective.components/Objective.js
@@ -5,35 +5,36 @@ import ObjectiveCard from './ObjectiveCard';
 import objectiveData from '../../../../assets/resume_eng.json';
 // import objectiveData from '../../../../assets/resume_dk.json';
 
-import axios from 'axios';
+// import axios from 'axios';
 
 
-import { useTranslation, Trans, withTranslation } from 'react-i18next';
+// import { useTranslation, Trans, withTranslation } from 'react-i18next';
 
 
-const Objective = () => {
+const Objective = (props) => {
 
-    const [objective, setObjective] = useState(null)
+    // const [objective, setObjective] = useState(null)
 
-    useEffect(() => {
-        // fetch('http://localhost:9000/objective')
-        // .then(response => response.json())
-        // .then(data =>  setObjective(data))
-        axios.get('http://localhost:9000/objective')
-            .then(response => setObjective(response.data))
-    }, [])
+    // useEffect(() => {
+    //     // fetch('http://localhost:9000/objective')
+    //     // .then(response => response.json())
+    //     // .then(data =>  setObjective(data))
+    //     axios.get('http://localhost:9000/objective')
+    //         .then(response => setObjective(response.data))
+    // }, [])
 
-    console.log('hello obj', objective)
+    // console.log('hello obj', objective)
 
     
 
-    const {t, i18n} = useTranslation();
+    // const {t, i18n} = useTranslation();
 
     return (
         <div className="objectiveSection">
             {/* <h2>{t('danish.hello')}</h2> */}
             {/* <Trans t={t}> */}
-                { objectiveData.objective.map((o, index) => {
+                {/* { objectiveData.objective.map((o, index) => { */}
+                { props.objective.map((o, index) => {
                 {/* {objective && objective.map((o, index) => { */}
                     return(
                         <ObjectiveCard
