3f42a8eefc202816eb6693f2e058d1d402d1b56a
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-09-22 10:12:42 +0200

    changes made

--- src/components/resume.components/components/skills.components/Skills.js
+++ src/components/resume.components/components/skills.components/Skills.js
@@ -2,19 +2,20 @@ import React from 'react'
 import SkillCard from './SkillCard';
 import './skill.styles.css';
 
-import skillsData from '../../../../assets/resume_eng.json';
+// import skillsData from '../../../../assets/resume_eng.json';
 // import skillsData from '../../../../assets/resume_dk.json';
 
-const Skills = () => {
+const Skills = (props) => {
     // const skillsData = [  'HTML', 
     // 'p', 'Stor ''Swagge',
     // 'TDD', 'P
 
     return (
         <div className="cvSkillSection">
-            <h3 className='cvSkillsTitle'>{skillsData.sectionTitles[4]}</h3>
+            <h3 className='cvSkillsTitle'>{props.sectionTitles[4]}</h3>
             <div className="cvSkillsContainer">
-                {skillsData.skills.map((s, index) => {
+                {/* {skillsData.skills.map((s, index) => { */}
+                {props.skills.map((s, index) => {
                 return( 
                         <SkillCard 
                             skill={s}
