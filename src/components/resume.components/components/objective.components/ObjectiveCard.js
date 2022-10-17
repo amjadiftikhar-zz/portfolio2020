4b5560ed8c33f6fa80c5b45d3c16e2358f324c7c
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-10-17 23:23:30 +0200

    changes made

--- src/components/resume.components/components/objective.components/ObjectiveCard.js
+++ src/components/resume.components/components/objective.components/ObjectiveCard.js
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 const ObjectiveCard = (props) => {
 
-    const {t, i18n} = useTranslation();
+    // const {t, i18n} = useTranslation();
 
     return (
         <div className="objectiveCard">
