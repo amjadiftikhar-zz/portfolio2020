d356fcbd2ee959be81fcd4b9c9963ea98b46d1ec
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-10-19 12:42:41 +0200

    changes made

--- src/components/resume.components/components/experience.components/VolunteerExperience.js
+++ src/components/resume.components/components/experience.components/VolunteerExperience.js
@@ -5,7 +5,7 @@ import './workExperience.styles.css';
 // import volunteerExperienceData from '../../../../assets/resume_dk.json';
 
 
-// export const volunteerExperieData = [
+// export const volunteerxperieData = [
 //     { 
 //tdeveloper.dk/',
 //         duratio: '11/20 -
