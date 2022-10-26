47b2bce6b8995396b112bea94cf27e0d7abc2ec9
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2022-10-26 22:37:08 +0200

    changes made

--- src/components/resume.components/components/experience.components/WorkExperience.js
+++ src/components/resume.components/components/experience.components/WorkExperience.js
@@ -14,7 +14,7 @@ import './workExperience.styles.css';
 //         task: 'Tasks:',
 //         taskDetails: [
 //             'Responsible for hat hosts different tools f',
-//             ' Planning and setting up frontend architecture, diagnosing, troubleshooting and resolving technical issues',
+//             ' Planning and settig up frontend architecture, diagnosing, troubleshooting and resolving technical issues',
 //             'Developing design and claboration with the design team',
 //             'Making reusable components and wrig test using React test
 //     {
